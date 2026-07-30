13de69f365e3b9104930708f327d943f3397934f
pranavkumarpk01 <pranav@PRANAVs-MacBook-Air.local>
2026-07-30 22:23:02 +0530

    hello world

---
+++ Python/add.py
@@ -0,0 +1,8 @@
+#Requirement -> we need to take two numbers as a input from the user in the form form of integer as datatype
+#Then with  the use of + (addition) parameter add those two numbers..
+
+num1 = int(input("Enter your first number: "))
+num2 = int(input("Enter your second number: "))
+
+sum = num1 + num2
+print("Sum =", sum)
---
+++ Python/hello.py
@@ -0,0 +1,7 @@
+# print("Hello world")
+# print("Hi this is Pranav")
+# print("Hi hope your doing well")
+
+
+name = input("Enter your name: ")
+print ("Welcome" , name)
