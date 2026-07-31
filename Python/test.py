6336541c1ec5d6cedb08607f417698866f3a8bf2
pranavkumarpk01 <pranav@PRANAVs-MacBook-Air.local>
2026-07-31 22:26:04 +0530

    how to pull the code

--- Python/test.py
+++ Python/test.py
@@ -1,1 +1,9 @@
-This is a test file
+#This is a test file
+#How to use my code
+#First if u r working for the first time use git clone command 
+# git clone https://github.com/pranavkumarpk01/GenAI-Jul.git
+
+# from next time in the same place or directory where u ran git clone comamnd 
+# git pull origin main
+
+#After running git pull origin main once from next time following that u can jsut run git pull
